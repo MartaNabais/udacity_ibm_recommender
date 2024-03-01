05cb8569efbff2001804c2847adb99d4ec5bbe07
MartaNabais <marta.nabais@gov.scot>
2024-03-01 18:40:51 +0000

    added functionality to main.py

--- src/main.py
+++ src/main.py
@@ -13,17 +13,36 @@ def main():
     """
     logger.info('IBM Recommender System')
     logger.info('Reading and cleaning data.')
-    data.cleaning_pipeline.extract_transform_load(args.user_item_csv,
-                                                  args.article_content_csv,
-                                                  args.db_filepath,
-                                                  args.user_item_table,
-                                                  args.article_content_table)
+    data.cleaning_pipeline.extract_transform_load('src/data/user-item-interactions.csv',
+                                                  'src/data/articles_community.csv',
+                                                  'src/data/ibm_recommender.db',
+                                                  'user_item_table',
+                                                  'article_content_table')
 
-    df = data.reader.read_db(args.db_filepath, args.user_item_table)
-    df_content = data.reader.read_db(args.db_filepath, args.article_content_table)
-    logger.info('')
-    print(df.head())
-    print(df_content.head())
+    df_articles = data.reader.read_db('src/data/ibm_recommender.db', 'user_item_table')
+    print(df_articles.head())
+    df_content = data.reader.read_db('src/data/ibm_recommender.db', 'article_content_table')
+
+    # Content based recommendation
+    logger.info('Performing content-based recommendations.')
+    articles_df = recommenders.content_based_recommender.intersect_articles(df=df_articles, df_content=df_content)
+    count_vect_df = recommenders.content_based_recommender.tfidf_vectorizer(articles_df=articles_df)
+    articles_similarity_df = recommenders.helper_functions.compute_similarity_df(df=count_vect_df)
+    recs = recommenders.helper_functions.content_rec(article_id=args.user_id,
+                                                     similarity_df=articles_similarity_df,
+                                                     df=articles_df)
+    logger.info('Content-based recommendation for user {}: \n {}'.format(args.user_id, recs))
+
+    logger.info('Performing SVD-based recommendations.')
+    # SVD-based recommendations
+    user_item = recommenders.helper_functions.create_user_item_matrix(df=df_articles)
+    svd_df = recommenders.svd_recommender.get_vt(user_item=user_item)
+    svd_similarity_df = recommenders.helper_functions.compute_similarity_df(df=svd_df.T)
+    print(svd_similarity_df)
+    recs = recommenders.helper_functions.content_rec(article_id=args.user_id,
+                                                     similarity_df=svd_similarity_df,
+                                                     df=articles_df)
+    logger.info('SVD-based recommendation for user {}: \n {}'.format(args.user_id, recs))
 
 
 if __name__ == "__main__":
@@ -40,11 +59,7 @@ if __name__ == "__main__":
         description='This program creates recommender system for IBM files.',
         epilog='Text at the bottom of help')
 
-    parser.add_argument('user_item_csv')
-    parser.add_argument('article_content_csv')
-    parser.add_argument('db_filepath')
-    parser.add_argument('user_item_table')
-    parser.add_argument('article_content_table')
+    parser.add_argument('user_id')
 
     args = parser.parse_args()
 
@@ -58,5 +73,8 @@ if __name__ == "__main__":
     # Modules
     import data.reader
     import data.cleaning_pipeline
+    import recommenders.content_based_recommender
+    import recommenders.helper_functions
+    import recommenders.svd_recommender
 
     main()
